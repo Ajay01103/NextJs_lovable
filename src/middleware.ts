be1c569c10025a05301c336c7e2d9de5ffe44859
Ajay <ajaysingh01103@gmail.com>
2025-10-30 20:55:01 +0530

    billing

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
+  "/pricing",
   "/sign-up(.*)",
   "/api/(.*)",
 ])
--- src/modules/projects/server/procedure.ts
+++ src/modules/projects/server/procedure.ts
@@ -3,6 +3,7 @@ import { generateSlug } from "random-word-slugs"
 import { z } from "zod/v4"
 import { prisma } from "@/db"
 import { inngest } from "@/inngest/client"
+import { consumeCredits } from "@/lib/usage"
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init"
 
 export const projectsRouter = createTRPCRouter({
@@ -16,6 +17,22 @@ export const projectsRouter = createTRPCRouter({
       })
     )
     .mutation(async ({ input, ctx }) => {
+      try {
+        await consumeCredits()
+      } catch (error) {
+        if (error instanceof Error) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: "Something went wrong",
+          })
+        } else {
+          throw new TRPCError({
+            code: "TOO_MANY_REQUESTS",
+            message: "You have run out of credits",
+          })
+        }
+      }
+
       const createdProject = await prisma.project.create({
         data: {
           userId: ctx.auth.userId,
